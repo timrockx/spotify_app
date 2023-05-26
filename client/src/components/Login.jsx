3505b87bb44da5e2467e9d823397d6a420bd13f1
timrockx <timothyyinlee@gmail.com>
2023-05-26 12:10:05 -0400

    login screen animation

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { motion } from "framer-motion";
 
 export default function Login() {
 
@@ -13,10 +13,20 @@ export default function Login() {
 
   return (
     <div className="min-h-screen bg-[#000] flex flex-col justify-center items-center">
-        <h1 className='py-12 text-3xl text-white font-gotham font-semibold'>Connect Your Spotify Account</h1>
-        <button className='h-1 w-44 rounded-full text-white font-medium bg-[#1DB954] p-8 flex items-center justify-center text-lg shadow-md shadow-[#7E7E7E] hover:bg-[#1ED760]'>
-          <a href={LOGIN_URI} className='hover:shadow-2xl hover: shadow-inner'>Log In</a>
-        </button>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1.3 }} 
+        >
+          <div className='flex flex-col justify-center items-center'>
+            <h1 className='py-12 text-3xl text-white font-gotham font-semibold'>Connect Your Spotify Account</h1>
+            <button className='h-1 w-44 rounded-full text-white font-medium bg-[#1DB954] p-8 flex items-center justify-center text-lg shadow-md shadow-[#7E7E7E] hover:bg-[#1ED760]'>
+              <a href={LOGIN_URI} className='hover:shadow-2xl hover: shadow-inner'>Log In</a>
+            </button>
+          </div>
+
+        </motion.div>
+
     </div>
   )
 }
