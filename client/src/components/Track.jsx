5e94e81953ba49fd45505ce02d9facf0aae6b674
timrockx <timothyyinlee@gmail.com>
2023-05-27 19:54:10 -0400

    add button for recommended songs

--- client/src/components/Track.jsx
+++ client/src/components/Track.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Avatar } from '@mui/material';
+import { Avatar, Icon } from '@mui/material';
+import axios from 'axios';
+import IconButton from '@mui/material/IconButton';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
-export default function Track({ name, link, album, image, artists, length, popularity }) {
+export default function Track({ id, name, link, album, image, artists, length, popularity }) {
 
     const msToMinutesAndSeconds = (ms) => {
         var minutes = Math.floor(ms / 60000);
         var seconds = ((ms % 60000) / 1000).toFixed(0);
         return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+
+    let backendURL = 'https://spotify-app-server.onrender.com';
+
+    if(process.env.NODE_ENV !== 'production') {
+        backendURL = 'http://localhost:8888';
+    }
+
+    const addTrack = (id) => {
+        
+        axios.get(`${backendURL}/add-track/${id.id}`)
+            .then(res => {
+                console.log("🚀 ~ file: Track.jsx ~ addTrack ~ res", res)
+            })
+            .catch(err => {
+                console.log("🚀 ~ file: Track.jsx ~ addTrack ~ err", err)
+            })
     }
 
   return (
@@ -20,7 +40,7 @@ export default function Track({ name, link, album, image, artists, length, popul
                 <a href={link} target='_blank' className='hover:text-[#1ED760]'>{name}</a>
             </h1>
 
-            <div className='flex flex-row justify-start items-center'>
+            <div className='hidden md:flex flex-row justify-start items-center'>
                 {artists?.map((artist, index) => {
                     return (
                         <div key={index}>
@@ -36,8 +56,10 @@ export default function Track({ name, link, album, image, artists, length, popul
         </div>
 
         {!length  ? 
-        <div className='text-white flex justify-center items-center px-5'>
-            <h1>{popularity}</h1>
+        <div className='text-white flex flex-col justify-center items-center p-5'>
+            <IconButton onClick={() => {addTrack({id})}} >
+                <AddCircleOutlineIcon sx={{ color: 'white', "&:hover": {color: '#1ED760', scale: '1.3'} }} />
+            </IconButton>
         </div>
         :
         <div className='text-white flex justify-center items-center px-5'>
