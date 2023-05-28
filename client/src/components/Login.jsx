8c2c47a31ef00f35968743d097805af71a341350
timrockx <timothyyinlee@gmail.com>
2023-05-27 20:14:40 -0400

    styling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -23,6 +23,7 @@ export default function Login() {
             <button className='h-1 w-44 rounded-full text-white font-medium bg-[#1DB954] p-8 flex items-center justify-center text-lg shadow-md shadow-[#7E7E7E] hover:bg-[#1ED760]'>
               <a href={LOGIN_URI} className='hover:shadow-2xl hover: shadow-inner'>Log In</a>
             </button>
+            <h3 className='italic text-white pt-5'>* Access is currently restricted because the app is in development mode.</h3>
           </div>
 
         </motion.div>
