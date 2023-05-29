294d28678206b67d1066d0a7f91b38f38df19a30
timrockx <timothyyinlee@gmail.com>
2023-05-28 22:56:09 -0400

    indent tags

--- client/src/components/Track.jsx
+++ client/src/components/Track.jsx
@@ -5,7 +5,7 @@ import IconButton from '@mui/material/IconButton';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { ToastContainer, toast } from 'react-toastify';
 
-export default function Track({ id, name, link, album, image, artists, length, popularity }) {
+export default function Track({ id, name, link, album, image, artists, length, isRecommendation }) {
 
     const msToMinutesAndSeconds = (ms) => {
         var minutes = Math.floor(ms / 60000);
@@ -59,46 +59,46 @@ export default function Track({ id, name, link, album, image, artists, length, p
 
   return (
     <div>
-    <div className='px-24 w-screen flex flex-row shrink-0 justify-start items-center m-auto rounded-lg shadow-2xl'>
-        <Avatar alt={name} src={image} variant='square' sx={{ width: 72, height: 72 }}/>
+        <div className='px-24 w-screen flex flex-row shrink-0 justify-start items-center m-auto rounded-lg shadow-2xl'>
+            <Avatar alt={name} src={image} variant='square' sx={{ width: 72, height: 72 }}/>
 
-        <div className='flex flex-col justify-start items-start w-full p-5'>
+            <div className='flex flex-col justify-start items-start w-full p-5'>
 
 
-            <h1 className='text-white text-xl py-2'>
-                <a href={link} target='_blank' className='hover:text-[#1ED760]'>{name}</a>
-            </h1>
+                <h1 className='text-white text-xl py-2'>
+                    <a href={link} target='_blank' className='hover:text-[#1ED760]'>{name}</a>
+                </h1>
 
-            <div className='hidden md:flex flex-row justify-start items-center'>
-                {artists?.map((artist, index) => {
-                    return (
-                        <div key={index}>
-                            <h2 className='text-white text-md px-1'>
-                                <a href={artist.external_urls.spotify} target='_blank' className='hover:underline hover:text-[#1ED760]'>{artist.name}</a>
-                            </h2>
-                        </div>
-                    )
-                })}
-                
-                <h1 className='text-white text-md'>&#183; {album}</h1>
+                <div className='hidden md:flex flex-row justify-start items-center'>
+                    {artists?.map((artist, index) => {
+                        return (
+                            <div key={index}>
+                                <h2 className='text-white text-md px-1'>
+                                    <a href={artist.external_urls.spotify} target='_blank' className='hover:underline hover:text-[#1ED760]'>{artist.name}</a>
+                                </h2>
+                            </div>
+                        )
+                    })}
+                    
+                    <h1 className='text-white text-md'>&#183; {album}</h1>
+                </div>
             </div>
-        </div>
 
-        {!length  ? 
-        <div className='text-white flex flex-col justify-center items-center p-5'>
-            <IconButton onClick={() => {addTrack({id})}} >
-                <AddCircleOutlineIcon sx={{ color: 'white', "&:hover": {color: '#1ED760', scale: '1.3'} }} />
-            </IconButton>
-        </div>
-        :
-        <div className='text-white flex justify-center items-center px-5'>
-            <h1>{msToMinutesAndSeconds(length)}</h1>
-        </div>
-        }
+            {!length  ? 
+            <div className='text-white flex flex-col justify-center items-center p-5'>
+                <IconButton onClick={() => {addTrack({id})}} >
+                    <AddCircleOutlineIcon sx={{ color: 'white', "&:hover": {color: '#1ED760', scale: '1.3'} }} />
+                </IconButton>
+            </div>
+            :
+            <div className='text-white flex justify-center items-center px-5'>
+                <h1>{msToMinutesAndSeconds(length)}</h1>
+            </div>
+            }
 
-        
-    </div>
-    <ToastContainer />
+            
+        </div>
+        <ToastContainer />
     </div>
   )
 }
