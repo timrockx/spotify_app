913178afb707817f0dee6afe72f9f662a20cb7e0
timrockx <timothyyinlee@gmail.com>
2023-05-29 20:53:43 -0400

    tailwind responsive screen sz

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,25 @@ module.exports = {
       'serif': ['ui-serif', 'Georgia'],
       'mono': ['ui-monospace', 'SFMono-Regular'],
       'gotham': ['Gotham', 'sans-serif'],
+    },
+    screens: {
+      'xs': '475px',
+      // => @media (min-width: 475px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [],
